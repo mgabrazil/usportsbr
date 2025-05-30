5779263bcbe9c8df4d265dc619eb054ca9a4d59c
mgabrazil <ti@mgabrazil.com>
2025-05-30 14:59:28 -0300

    update folders

---
+++ src/pages/Home/index.js
@@ -0,0 +1,31 @@
+function Home(){
+    
+    return(
+        
+        <section className="container">
+            <div>
+                <div className="logo-container">
+                    <img src="#" className="logo" />
+                </div>
+
+                <div className="menu-container">
+                    <span>Tecidos</span>
+                    <span>Fabricacao</span>
+                    <span>Design</span>
+                </div>
+
+                <div>
+                    <span class="material-symbols-outlined">search</span>
+
+                    <span>Parceiros</span>
+                    <span class="material-symbols-outlined">arrow_drop_down</span>
+                </div>
+            </div>
+
+            
+
+        </section>
+    )
+}
+
+export default Home;
