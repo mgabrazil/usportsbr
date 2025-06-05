2347f16e1c3c3c0ab3f3cc7b15d734db6f746750
Guilherme Brasil <guilherme.brasil@sempreceub.com>
2025-06-05 13:50:35 -0300

    Update: Slide animation

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,6 @@
+// src/pages/Home/index.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import { useEffect, useState } from 'react';
 
 import shirt from '../../assets/img-tshirt.png';
@@ -10,87 +10,89 @@ import imagemMulher from '../../assets/img-woman-fita.png';
 import '../../styles/home2.css';
 import '../../styles/home.css';
 
-function Home(){
+function Home() {
+  const [currentPage, setCurrentPage] = useState(1);
 
-    const [currentPage, setCurrentPage] = useState(1);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCurrentPage((prev) => (prev === 1 ? 2 : 1));
+    }, 1000000);
+    return () => clearTimeout(timer);
+  }, [currentPage]);
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setCurrentPage(prev => (prev === 1 ? 2 : 1));
-        }, 10000000);
+  const showPrevious = () => {
+    setCurrentPage(1);
+  };
 
-        return () => clearTimeout(timer);
-    }, [currentPage]);
+  const showNext = () => {
+    setCurrentPage(2);
+  };
 
-    const showPrevious = () => {
-        setCurrentPage(1);
-    }
+  const sliderTranslate = `translateX(-${(currentPage - 1) * 100}vw)`;
 
-    const showNext = () => {
-        setCurrentPage(2);
-    }
-
-
-    return(
-        
-        <section className="home-container-base">
-
-                        {currentPage === 1 && (
-                <section className="home2-container">
-                    <main>
-                        <span className="material-symbols-outlined" id="right-arrow" onClick={showNext}>double_arrow</span>
-
-                            <div className="container-imagens">
-                            <div className="homem">
-                                <img src={imagemHomem} alt="Homem" />
-                            </div>
-
-                            <div className="mulher">
-                                <img src={imagemMulher} alt="Mulher" />
-                            </div>
-                            </div>
-
-                            <div className="caixa-texto">
-                            <h1 className="titulo">DESIGN EXCLUSIVO</h1>
-                            <p className="subtitulo">
-                                Entre em contato com a nossa equipe.
-                            </p>
-                            </div>
-                    </main>
-                </section>
-            )}
-
-            {currentPage === 2 && (
-                <section className="home1-container">
-
-                    <span className="material-symbols-outlined" id="left-arrow" onClick={showPrevious}>double_arrow</span>
-                    <span className="material-symbols-outlined" id="right-arrow" onClick={showPrevious}>double_arrow</span>
-
-                    <main className='body-container'>
-
-                        <div className='tshirt'>
-                            <img src={shirt} />
-                        </div>
-
-                        <div className='text-container'>
-                            <div className='title'>
-                                <h1 className='title-top'>SEU TIME,</h1>
-                                <h1 className='title-bottom'>SUA IDENTIDADE</h1>
-                            </div>
-
-                            <div className='phrase-container'>
-                                <p>Uniformes personalizados </p>
-                                <p>com qualidade e entrega rápida</p>
-                            </div>
-
-                            <Link to='#' className='btn-price'>CLIQUE AQUI E FAÇA SEU ORÇAMENTO</Link>
-                        </div>
-
-                    </main>
-                </section> 
-            )}   
+  return (
+    <section className="home-container-base">
+      <div className="slider" style={{ transform: sliderTranslate }}>
+        <section className="home2-container slide">
+          <main>
+            <span
+              className="material-symbols-outlined"
+              id="right-arrow"
+              onClick={showNext}
+            >
+              double_arrow
+            </span>
+            <div className="container-imagens">
+              <div className="homem">
+                <img src={imagemHomem} alt="Homem" />
+              </div>
+              <div className="mulher">
+                <img src={imagemMulher} alt="Mulher" />
+              </div>
+            </div>
+            <div className="caixa-texto">
+              <h1 className="titulo">DESIGN EXCLUSIVO</h1>
+              <p className="subtitulo">Entre em contato com a nossa equipe.</p>
+            </div>
+          </main>
         </section>
-    );
+        <section className="home1-container slide">
+          <span
+            className="material-symbols-outlined"
+            id="left-arrow"
+            onClick={showPrevious}
+          >
+            double_arrow
+          </span>
+          <span
+            className="material-symbols-outlined"
+            id="right-arrow"
+            onClick={showPrevious}
+          >
+            double_arrow
+          </span>
+          <main className="body-container">
+            <div className="tshirt">
+              <img src={shirt} />
+            </div>
+            <div className="text-container">
+              <div className="title">
+                <h1 className="title-top">SEU TIME,</h1>
+                <h1 className="title-bottom">SUA IDENTIDADE</h1>
+              </div>
+              <div className="phrase-container">
+                <p>Uniformes personalizados </p>
+                <p>com qualidade e entrega rápida</p>
+              </div>
+              <Link to="#" className="btn-price">
+                CLIQUE AQUI E FAÇA SEU ORÇAMENTO
+              </Link>
+            </div>
+          </main>
+        </section>
+      </div>
+    </section>
+  );
 }
 
 export default Home;
