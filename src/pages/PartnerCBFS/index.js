bdfb892169a5b0202b2ac2ec7a43ac5b7a5306c9
mgabrazil <ti@mgabrazil.com>
2025-06-05 13:25:00 -0300

    CREATE paginas Parceiros

---
+++ src/pages/PartnerCBFS/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import casaco4 from '../../assets/CBFS_CASACO_4.png';
+
+export default function PartnerCBFS(){
+    return(
+        <section className="cbfs-container-base">
+            <div className="cbfs-container">
+                <img src={casaco4} id="background-casaco4" />
+            </div>
+        </section>
+    )
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,15 +6,29 @@ import Design from '../pages/Design';
 import Tecidos from '../pages/Tecidos';
 import SearchResults from '../pages/SearchResults';
 
+import PartnerADEF from '../pages/PartnerADEF';
+import PartnerCBFS from '../pages/PartnerCBFS';
+import PartnerSUB17 from '../pages/PartnerSUB17';
+import PartnerLFF from '../pages/PartnerLFF';
+
 export default function RoutesApp(){
     return(
         <Routes>
+            
             <Route path='/' element={ <Home/> } />
             <Route path='/tecidos' element={ <Tecidos/> } />
             <Route path='/fabricacao' element={ <Fabricacao/> } />
             <Route path='/design' element={ <Design/> } />
 
+
             <Route path='/search' element={ <SearchResults/> } />
+
+
+            <Route path='/adef' element={ <PartnerADEF/> } />
+            <Route path='/cbfs' element={ <PartnerCBFS/> } />
+            <Route path='/sub17' element={ <PartnerSUB17/> } />
+            <Route path='/lff' element={ <PartnerLFF/> } />
+
         </Routes>
     )
 }
