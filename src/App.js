812b755feb54b92f6ce098b8f58acc18b31e41ac
mgabrazil <ti@mgabrazil.com>
2025-06-10 09:52:19 -0300

    ADD basename on App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename='/usportsbr'>
         <Header/>
         <WhatsappBtn/>
         <RoutesApp/>
