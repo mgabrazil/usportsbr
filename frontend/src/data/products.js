7fd38a21b39ec6d8d2a2518331bc2fa93131affd
mgabrazil <ti@mgabrazil.com>
2025-06-11 12:51:57 -0300

    UPDATE branches

--- frontend/src/data/products.js
+++
@@ -1,7 +0,0 @@
-// VALORES HIPOTETICOS
-const products = [
-    { id: 1, name: "Camisa Teste", category: "Categoria Teste" },
-    { id: 2, name: "Camisa Teste 2", category: "Categoria Teste 2" }
-]
-
-export default products;
