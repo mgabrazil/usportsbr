43f678c947218e165654a67409530003853b57e2
mgabrazil <ti@mgabrazil.com>
2025-06-11 14:37:33 -0300

    UPDATE basename

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter } from 'react-router-dom';
 import RoutesApp from './routes';
 
-import Header from '../src/components/Header';
+import Header from './components/Header';
 import WhatsappBtn from './components/WhatsappBtn';
 
 import './App.css';
@@ -10,7 +10,7 @@ function App() {
   return (
     
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Header/>
         <WhatsappBtn/>
         <RoutesApp/>
