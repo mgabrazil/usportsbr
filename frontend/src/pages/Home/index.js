4f8292de719de0d5c8dc12d0dfd50f798c086722
Guilherme Brasil <guilherme.brasil@sempreceub.com>
2025-06-26 16:14:58 -0300

    Slide timer alteration

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -23,7 +23,7 @@ function Home() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setCurrentPage(prev => (prev === 1 ? 2 : 1));
-    }, 150000);
+    }, 15000);
     return () => clearTimeout(timer);
   }, [currentPage]);
 
