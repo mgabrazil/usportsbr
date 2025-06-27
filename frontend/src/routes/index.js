4ab2c6217101c0a3dd354a8036953e30fd9abe5a
mgabrazil <ti@mgabrazil.com>
2025-06-27 14:06:03 -0300

    CREATE landing presentation page and UPDATE contact for desktop

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -5,6 +5,7 @@ import Fabricacao from '../pages/Fabricacao';
 import Design from '../pages/Design';
 import Tecidos from '../pages/Tecidos';
 import SearchResults from '../pages/SearchResults';
+import Landing from '../pages/Landing';
 
 import PartnerADEF from '../pages/PartnerADEF';
 import PartnerCBFS from '../pages/PartnerCBFS';
@@ -20,14 +21,16 @@ export default function RoutesApp(){
             <Route path='/fabricacao' element={ <Fabricacao/> } />
             <Route path='/design' element={ <Design/> } />
 
-
+            {/* PAGINA SEARCH COMENTADA
             <Route path='/search' element={ <SearchResults/> } />
-
+            */}
 
             <Route path='/adef' element={ <PartnerADEF/> } />
             <Route path='/cbfs' element={ <PartnerCBFS/> } />
             <Route path='/sub17' element={ <PartnerSUB17/> } />
             <Route path='/lff' element={ <PartnerLFF/> } />
+
+            <Route path='/apresentacao' element={ <Landing/> } />
 
         </Routes>
     )
