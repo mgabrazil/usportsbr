1613882fc9614c21f2e05e3791fd4536395ceb59
Guilherme Brasil <guilherme.brasil@sempreceub.com>
2025-07-08 13:34:00 -0300

    Fix Positions

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -25,7 +25,7 @@ function Home() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setCurrentPage(prev => (prev === 1 ? 2 : 1));
-    }, 150000);
+    }, 15000);
     return () => clearTimeout(timer);
   }, [currentPage]);
   const showPrevious = () => setCurrentPage(1);
